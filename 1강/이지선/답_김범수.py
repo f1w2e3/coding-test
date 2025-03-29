93d2c53c043e942e4361df3553c42c529ea6ce88
김범수 <rlatmddnjs4079@naver.com>
2025-03-29 16:04:43 +0900

    fdsfsdfsd

---
+++ 1강/이지선/답_김범수.py
@@ -0,0 +1,29 @@
+#문제1: 그리디 알고리즘
+# 파이썬 코드를 실행했을 때 출력 결과는?
+#money = 370
+#coins = [100, 50, 10]
+#count = 0
+
+#for coin in coins:
+    #count += money // coin
+    #money %= coin
+
+#print(count)
+
+
+답: 6
+
+#문제2: 구현
+# 다음 코드의 출력 결과는?
+#text = "hello"
+#count = 0
+
+#for ch in text:
+#    if ch in "aeiou":
+#        count += 1
+
+#print(count)
+
+
+
+답: 2
