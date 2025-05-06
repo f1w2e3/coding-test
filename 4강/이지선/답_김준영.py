49284000ceff3fa32051b2e566c7164bd65721ee
f1w2e3 <daywedset@example.com>
2025-05-06 23:25:08 +0900

    1

--- 4강/이지선/답_김준영.py
+++ 4강/이지선/답_김준영.py
@@ -12,50 +12,50 @@
 #선택 정렬:
 #테스트용: array=[7,5,9,0,3,1,6,2,4,8]
 
-array=list(map(int,input().split(" ")))
+# array=list(map(int,input().split(" ")))
 
-def find_small_index(x):
-    #x부터 len(array) 까지 중 가장 작은 값의 인덱스 찾기
-    front=array[x]
-    index=x
-    for _ in range(x+1,len(array)):
-        if array[_]<front:
-              front=array[_]
-              index=_
-    return index
+# def find_small_index(x):
+#     #x부터 len(array) 까지 중 가장 작은 값의 인덱스 찾기
+#     front=array[x]
+#     index=x
+#     for _ in range(x+1,len(array)):
+#         if array[_]<front:
+#               front=array[_]
+#               index=_
+#     return index
 
-for x in range(len(array)):
-    temp=array[x]
-    small_index=find_small_index(x)
-    array[x]=array[small_index] #x+1부터 len(array)까지 중 가장 작은 애 
-    array[small_index]=temp
+# for x in range(len(array)):
+#     temp=array[x]
+#     small_index=find_small_index(x)
+#     array[x]=array[small_index] #x+1부터 len(array)까지 중 가장 작은 애 
+#     array[small_index]=temp
 
-print(array)
-
-
-
-문제1:
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[0]  
-    left = [x for x in arr[1:] if x <= pivot]  
-    right = [x for x in arr[1:] if x > pivot]  
-    return quick_sort(left) + [pivot] + quick_sort(right)
-
-array = list(map(int, input().split()))
-print(quick_sort(array)) 
+# print(array)
 
 
-문제2:
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        for j in range(i, 0, -1): 
-            if arr[j] < arr[j-1]:
-                arr[j], arr[j-1] = arr[j-1], arr[j]
-            else:
-                break
-    return arr
 
-array = list(map(int, input().split()))  
-print(insertion_sort(array)) 
+# 문제1:
+# def quick_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     pivot = arr[0]  
+#     left = [x for x in arr[1:] if x <= pivot]  
+#     right = [x for x in arr[1:] if x > pivot]  
+#     return quick_sort(left) + [pivot] + quick_sort(right)
+
+# array = list(map(int, input().split()))
+# print(quick_sort(array)) 
+
+
+# 문제2:
+# def insertion_sort(arr):
+#     for i in range(1, len(arr)):
+#         for j in range(i, 0, -1): 
+#             if arr[j] < arr[j-1]:
+#                 arr[j], arr[j-1] = arr[j-1], arr[j]
+#             else:
+#                 break
+#     return arr
+
+# array = list(map(int, input().split()))  
+# print(insertion_sort(array)) 
